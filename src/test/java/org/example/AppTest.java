ee6b18eeb3b97b09903743d30806955232addc19
satyainv <satyainv459@gmail.com>
2022-09-14 22:47:27 +0200

    My first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    public static void main(String[] args) {
+        System.out.println("satya test class");
+    }
+}
